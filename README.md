6aba63421ded4667cba51afdba1f6f5189b4cd7e
WangYan <topshihun@foxmail.com>
2026-02-16 19:32:29 +0800

    feat: merge build_typst.py to build.py and rename posts_typst to posts

--- README.md
+++ README.md
@@ -5,11 +5,9 @@
 ## 项目结构
 
 - assets 全局资源文件
-- posts 由typst生成的博客文件，与posts_typst目录结构保持一致
-- posts_typst 原始的typst文件，与posts目录结构保持一致
+- posts 需要typst生成的博客文件
 - scripts python的模块文件
-- build.py 构建脚本，用于生成博客文件，将posts_typst生成posts
-- build_out.py 用于生抽out目录以及文件，生成的文件是必要的静态文件，方便静态上传部署
+- build.py 构建脚本，用于生成博客文件（out目录）
 - index.html 首页，用于展示博客列表等内容
 - pyproject.toml 项目配置文件，用于管理项目依赖和配置信息
 - README.md 说明文档，用于展示项目信息等内容
--- build.py
+++ build.py
@@ -1,32 +1,48 @@
 import os
+import shutil
 
-# remove posts files
-for root, dirs, files in os.walk("posts"):
-    for file in files:
-        if os.path.isfile(os.path.join(root, file)):
-            remove_file = os.path.join(root, file)
-            print(f"Removing {remove_file}")
-            os.remove(remove_file)
-# remove posts directory if it exists
-for root, dirs, files in os.walk("posts"):
-    for dir in dirs:
-        remove_dir = os.path.join(root, dir)
-        print(f"Removing {remove_dir}")
-        os.rmdir(remove_dir)
-print("Cleaned posts")
+from scripts.utils import err, info
 
-# compile all typst files in posts_typst to html and correct output file path(posts)
-for root, dirs, files in os.walk("posts_typst"):
+# clean out directory
+if os.path.exists("out"):
+    shutil.rmtree("out")
+    info("Removed existing out directory")
+os.makedirs("out")
+
+# compile all typst files in posts to html and correct output file path(out/posts)
+for root, dirs, files in os.walk("posts"):
     for file in files:
         if file.endswith(".typ"):
             compile_file = os.path.join(root, file)
             output_file = os.path.join(
-                root.replace("posts_typst", "posts"), file.replace(".typ", ".html")
+                root.replace("posts", "out/posts"), file.replace(".typ", ".html")
             )
             # create output directory if it doesn't exist
             os.makedirs(os.path.dirname(output_file), exist_ok=True)
-            print(f"Compiling {compile_file}")
+            info(f"Compiling {compile_file}")
             command = f"typst compile --features html --format html {compile_file} {output_file}"
-            print(f"Executing command: {command}")
+            info(f"Executing command: {command}")
             os.system(command)
-print("Compiled posts")
+info("Compiled posts")
+
+
+def copyDir(file):
+    if os.path.exists(file):
+        shutil.copytree(file, f"out/{file}")
+        info(f"{file} copied.")
+    else:
+        err(f"{file} not found.")
+
+
+def copyFile(file):
+    if os.path.exists(file):
+        shutil.copy(file, f"out/{file}")
+        info(f"{file} copied.")
+    else:
+        err(f"{file} not found.")
+
+
+copyDir("assets")
+copyFile("index.html")
+copyFile("favicon.ico")
+copyFile("robots.txt")
